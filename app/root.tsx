483fac44712b8e4fb09803a227d0e9cdde55da32
Tom Irimescu <tom@irimescu.com>
2023-07-03 16:28:57 -0400

    Clean up, turn off meta future flaf

--- app/root.tsx
+++ app/root.tsx
@@ -1,4 +1,3 @@
-// import { cssBundleHref } from '@remix-run/css-bundle';
 import type { MetaFunction, LinksFunction } from '@remix-run/node';
 import {
   Links,
@@ -11,13 +10,15 @@ import {
 
 import styles from './styles/app.css';
 
-// export const links: LinksFunction = () => [
-//   ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
-// ];
-
 export const links: LinksFunction = () => {
   return [{ rel: 'stylesheet', href: styles }];
 };
+
+export const meta: MetaFunction = () => ({
+  charset: 'utf-8',
+  title: 'New Remix App',
+  viewport: 'width=device-width,initial-scale=1',
+});
 
 export default function App() {
   return (
